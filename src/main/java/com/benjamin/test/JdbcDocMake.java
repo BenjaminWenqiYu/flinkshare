294ee73ff3c5eee94f0fd8a388c75b766d34d142
YuWenqi <337998325@qq.com>
2021-04-29 15:11:46 +0800

    添加数据库文档生成代码

---
+++ src/main/java/com/benjamin/test/JdbcDocMake.java
@@ -0,0 +1,84 @@
+package com.benjamin.test;
+
+import cn.smallbun.screw.core.Configuration;
+import cn.smallbun.screw.core.engine.EngineConfig;
+import cn.smallbun.screw.core.engine.EngineFileType;
+import cn.smallbun.screw.core.engine.EngineTemplateType;
+import cn.smallbun.screw.core.execute.DocumentationExecute;
+import cn.smallbun.screw.core.process.ProcessConfig;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ywq
+ * Date: 2021-04-29
+ * Time: 11:54
+ * Description:
+ */
+public class JdbcDocMake {
+    public void shouldAnswerWithTrue() {
+        // 数据源
+        HikariConfig hikariConfig = new HikariConfig();
+//        hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//        hikariConfig.setJdbcUrl("jdbc:mysql://127.0.0.1:3306/test");
+        hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+        hikariConfig.setJdbcUrl("jdbc:oracle:thin:@127.0.0.1:1521:test");
+        hikariConfig.setUsername("username");
+        hikariConfig.setPassword("password");
+        //设置可以获取tables remarks信息
+        hikariConfig.addDataSourceProperty("useInformationSchema", "true");
+        hikariConfig.setMinimumIdle(2);
+        hikariConfig.setMaximumPoolSize(5);
+        DataSource dataSource = new HikariDataSource(hikariConfig);
+        //生成配置
+        EngineConfig engineConfig = EngineConfig.builder()
+                //生成文件路径
+                .fileOutputDir("/Users/lengleng")
+                //打开目录
+                .openOutputDir(true)
+                //文件类型
+                .fileType(EngineFileType.MD)
+                //生成模板实现
+                .produceType(EngineTemplateType.freemarker).build();
+
+        //忽略表
+        ArrayList<String> ignoreTableName = new ArrayList<>();
+        ignoreTableName.add("test_user");
+        ignoreTableName.add("test_group");
+        //忽略表前缀
+        ArrayList<String> ignorePrefix = new ArrayList<>();
+        ignorePrefix.add("test_");
+        //忽略表后缀
+        ArrayList<String> ignoreSuffix = new ArrayList<>();
+        ignoreSuffix.add("_test");
+        ProcessConfig processConfig = ProcessConfig.builder()
+                //忽略表名
+                .ignoreTableName(ignoreTableName)
+                //忽略表前缀
+                .ignoreTablePrefix(ignorePrefix)
+                //忽略表后缀
+                .ignoreTableSuffix(ignoreSuffix).build();
+        //配置
+        Configuration config = Configuration.builder()
+                //版本
+                .version("1.0.0")
+                //描述
+                .description("数据库设计文档生成")
+                //数据源
+                .dataSource(dataSource)
+                //生成配置
+                .engineConfig(engineConfig)
+                //生成配置
+                .produceConfig(processConfig).build();
+        //执行生成
+        new DocumentationExecute(config).execute();
+    }
+
+    public static void main(String[] args) {
+        new JdbcDocMake().shouldAnswerWithTrue();
+    }
+}
